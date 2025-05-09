53c0361b28ec9ec50c542bf4298cd2cf609d5739
Ivanrizkys <ivnrzkys@gmail.com>
2025-05-09 13:40:58 +0700

    feat: add Vitest setup configuration and cleanup after each test

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+import { configDefaults, defineConfig } from "vitest/config";
+import viteConfig from "./vite.config";
+
+export default defineConfig({
+	...viteConfig,
+	test: {
+		globals: true,
+		environment: "jsdom",
+		setupFiles: "./src/test/setup.ts",
+	},
+});
