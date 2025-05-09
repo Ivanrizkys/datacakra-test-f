85aa757344816506fa55512d1c1aee936957d13e
Ivanrizkys <ivnrzkys@gmail.com>
2025-05-09 13:43:43 +0700

    feat: add authentication routes for login and register

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,23 @@
+import { Login } from "@/pages/Login";
+import { Register } from "@/pages/Register";
 import { createBrowserRouter } from "react-router";
 
 export const routes = createBrowserRouter([
 	{
-		path: "",
+		path: "/",
 		element: <div className="text-center">Landing Page Route</div>,
+	},
+	{
+		path: "/auth",
+		children: [
+			{
+				path: "login",
+				element: <Login />,
+			},
+			{
+				path: "register",
+				element: <Register />,
+			},
+		],
 	},
 ]);
