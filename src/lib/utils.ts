d1a88b47b475540c3b894b6ff14a311c551ea795
Ivanrizkys <ivnrzkys@gmail.com>
2025-05-10 15:58:52 +0700

    feat: add utility functions for generating initials and converting image URLs to File objects

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,37 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+/**
+ * Returns the uppercase initials of a full name to be used on a badge.
+ *
+ * @param fullName - The full name (e.g., "Ivan Rizky Saputra").
+ * @returns A string of uppercase initials (e.g., "IRS").
+ */
+export function getInitials(fullName: string): string {
+	return fullName
+		.trim()
+		.split(/\s+/) // Split by one or more spaces
+		.map((word) => word[0])
+		.join("")
+		.toUpperCase();
+}
+
+/**
+ * Converts an image URL into a File object.
+ * @param {string} imageUrl - The URL of the image to convert.
+ * @returns {Promise<File>} A Promise that resolves to a File object.
+ */
+export async function imageURLToFile(imageUrl: string): Promise<File> {
+	const response = await fetch(imageUrl);
+	const blob = await response.blob();
+
+	const url = new URL(imageUrl);
+	const pathname = url.pathname;
+	const fileName =
+		pathname.substring(pathname.lastIndexOf("/") + 1) || "image.jpg";
+
+	const contentType = blob.type || "image/jpeg";
+
+	return new File([blob], fileName, { type: contentType });
+}
