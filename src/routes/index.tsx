bbcf03e22ab8ca0bd4ab4762ee17135bddf2c52e
Ivanrizkys <ivnrzkys@gmail.com>
2025-05-10 15:59:08 +0700

    feat: implement routing for dashboard and article management with lazy loading

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,40 @@
+import { DashboardLayout } from "@/components/layout/Dashboard";
 import { Login } from "@/pages/Login";
 import { Register } from "@/pages/Register";
-import { createBrowserRouter } from "react-router";
+import { lazy } from "react";
+import { Navigate, createBrowserRouter } from "react-router";
+
+const ArticleList = lazy(() => import("@/pages/ArticleList"));
+const ArticleCreate = lazy(() => import("@/pages/ArticleCreate"));
+const ArticleUpdate = lazy(() => import("@/pages/ArticleUpdate"));
+const ArticleDetail = lazy(() => import("@/pages/ArticleDetail"));
 
 export const routes = createBrowserRouter([
 	{
 		path: "/",
-		element: <div className="text-center">Landing Page Route</div>,
+		element: <Navigate to="/dashboard/article" />,
+	},
+	{
+		path: "/dashboard",
+		element: <DashboardLayout />,
+		children: [
+			{
+				path: "article",
+				element: <ArticleList />,
+			},
+			{
+				path: "article/create",
+				element: <ArticleCreate />,
+			},
+			{
+				path: "article/update/:documentId",
+				element: <ArticleUpdate />,
+			},
+			{
+				path: "article/:documentId",
+				element: <ArticleDetail />,
+			},
+		],
 	},
 	{
 		path: "/auth",
